5d4a8a9b5b64fa4fa6db1a968541e1e8f502b360
oridim <125223080+oridim@users.noreply.github.com>
2024-12-05 18:09:59 +0000

    chore: added build script that focuses on only our player

---
+++ compile-player.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# **NOTE**: The below flag causes the Bash script to exit whenever a command fails.
+
+set -e
+
+# Compile only the osadon player to a shared object.
+
+g++ -shared -fPIC -ansi -pedantic -std=c++14 -Wall -g \
+    "./board.cxx" \
+    "./OzgeAkosa5177_DimitriNearchosdon5092.cxx" -o "./OzgeAkosa5177_DimitriNearchosdon5092.so"
